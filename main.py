e73e5a1763a1e0eab5db4998fe1db90218a7c659
Bardo Espinosa <bardoespinosaa@gmail.com>
2020-08-10 09:40:42 -0500

    repository update

--- main.py
+++ main.py
@@ -8,27 +8,68 @@ Created on Fri Jul  3 21:21:28 2020
 
 import praw
 import info
+import json
+from time import sleep
 
-reddit = praw.Reddit(client_id =info.client_id,
-                     client_secret = info.client_secret,
-                     username = info.username,
-                     password = info.password,
-                     user_agent = "me")
+running = True
+counter = 0
 
-print(reddit.random_subreddit())
-
-sub = reddit.subreddit("mexico")
-
-htmx = sub.hot(limit =30)
-
-for submi in htmx:
-    print(submi.author)
+#try except to read and wirite the file
+def main():
     try:
-        coms = submi.comments
-        for aus in coms:
-            print(aus.author)
+        with open('data/data.json', 'r') as myfile:
+            obj = myfile.read()
+            data=json.loads(obj.encoding("utf-8"))
+            print("+++++++++++++++++++ file open +++++++++++++++++++++++")
     except:
-        print("no comments")
+        data={}
+        data["subs"]=info.targets
+        for target in info.targets:
+            data[target] = []
+        print("----------------- open failed ----------------------")
+        
     
+    reddit = praw.Reddit(client_id =info.client_id,
+                         client_secret = info.client_secret,
+                         username = info.username,
+                         password = info.password,
+                         user_agent = "me")
     
-
+    print(reddit.random_subreddit())
+    
+    for subreddit in data["subs"]:
+    
+        sub = reddit.subreddit(subreddit)
+        
+        ht = sub.new(limit =30)
+        print("-------- scanning r/{} -------".format(subreddit))
+        for submi in ht:
+            try:
+                data[subreddit].append(str(submi.author))
+                
+                try:
+                    coms = submi.comments
+                    for aus in coms:
+                        data[subreddit].append(str(aus.author))
+                        #print(aus.author)
+                except:
+                    print("no comments")
+            except:
+                print("unable to perform action")
+        data[subreddit] = list(set(data[subreddit]))
+        print("------- {} users saved(total) -------".format(len(data[subreddit] )))
+        
+        
+    try:
+        with open('data/data.json', 'w') as outfile:
+            json.dump(data, outfile)
+    except:
+        print("Cannot save info")
+        
+while (running):
+    sleep(30)
+    main()
+    print("----- round {} -----".format(counter))
+    counter += 1
+    if counter == 10:
+        running=False
